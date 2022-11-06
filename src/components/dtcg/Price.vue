fdfb1534eddff297df945b7fff873dd3038184b8
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-11-07 00:37:49 +0800

    Update Price.vue

--- src/components/dtcg/Price.vue
+++ src/components/dtcg/Price.vue
@@ -3,6 +3,7 @@ import { ref } from "vue"
 import type { DeckPriceResp } from "../../api/v1/models/DeckPriceResp"
 
 let deck = ref<string>("")
+let hid = ref<string>("")
 let resp = ref<DeckPriceResp>({
   min_price: "",
   avg_price: "",
@@ -25,9 +26,25 @@ function commit(params: string) {
     console.log(resp.value)
   }
 }
+
+function commitTwo(params: string) {
+  console.log(params)
+  let url = "https://tcg.102205.xyz:8443/api/v1/deck/price/hid/" + params
+  let xhr = new XMLHttpRequest()
+  console.log(url)
+
+  xhr.open("GET", url)
+  xhr.send(null)
+  xhr.onload = function () {
+    resp.value = JSON.parse(xhr.responseText)
+    console.log(resp.value)
+  }
+}
 </script>
 
 <template>
+  <div>卡组HID：<input v-model="hid" type="text" /></div>
+  <button @click="commitTwo(hid)">提交</button>
   <div>
     <p>卡组：</p>
     <textarea
@@ -57,6 +74,7 @@ function commit(params: string) {
           <th>名称</th>
           <th>数量</th>
           <th>编号</th>
+          <th>异画</th>
           <th>最低价</th>
           <th>集换价</th>
         </tr>
@@ -66,6 +84,7 @@ function commit(params: string) {
           <th>{{ item.sc_name }}</th>
           <th>{{ item.count }}</th>
           <th>{{ item.serial }}</th>
+          <th>{{ item.alternative_art }}</th>
           <th>{{ item.min_price }}</th>
           <th>{{ item.avg_price }}</th>
         </tr>
