2ad21a658c6c950cf417a6069028cb86f85bccef
DesistDaydream <373406000@qq.com>
2022-11-07 16:24:01 +0800

    Update DeckPriceTable.vue

--- src/components/dtcg/DeckPriceTable.vue
+++ src/components/dtcg/DeckPriceTable.vue
@@ -8,6 +8,8 @@ let deck = ref<string>("")
 let hid = ref<string>("")
 let tableDataForDeckPriceResp = ref<DeckPriceResp>(NewDeckPriceResp())
 
+// 示例JSON：
+// ["Exported from http://digimon.card.moe","ST1-01","ST1-03","ST1-03","ST1-03","ST1-06","ST1-06","ST1-07","ST1-07","ST1-07","ST1-07","ST1-16","ST1-16","BT1-010","BT1-010","BT1-020","BT1-025","BT1-025","BT1-084","BT1-085","BT1-114","BT1-114","P-009","P-009","P-009","P-009","BT4-017","BT4-019","BT4-019","BT4-092","BT4-099","BT5-001","BT5-001","BT5-001","BT5-001","BT5-007","BT5-007","BT5-007","BT5-010","BT5-010","BT5-010","BT5-010","BT5-015","BT5-015","BT5-015","BT5-015","BT5-016","BT5-016","BT5-086","BT5-086","BT5-092","BT5-092","ST7-12","P-035","P-035","P-035"]
 function commitWithOfficialJSON(deck: string) {
   let req = { deck: deck, envir: "chs" }
   postDeckPrice(req).then((resp) => {
@@ -17,6 +19,7 @@ function commitWithOfficialJSON(deck: string) {
   })
 }
 
+// 示例HID：6cea907f6a001007281eaa8f52feb517a811a5bd
 function commitWithHID(hid: string) {
   let req = hid
   getDeckPriceWithHID(req).then((resp) => {
