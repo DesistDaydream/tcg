d118dc7d873a077be9fbc4096b2859596c1eca98
DesistDaydream <373406000@qq.com>
2022-11-08 10:09:49 +0800

    Update DeckPriceTable.vue

--- src/components/dtcg/DeckPriceTable.vue
+++ src/components/dtcg/DeckPriceTable.vue
@@ -4,14 +4,14 @@ import type { DeckPriceResp, Data } from "@/api/v1/models/DeckPriceResp"
 import { NewDeckPriceResp } from "@/api/v1/models/DeckPriceResp"
 import { postDeckPrice, getDeckPriceWithHID } from "@/api/v1/services"
 
-let deck = ref<string>("")
-let hid = ref<string>("")
+// 表格中的数据
 let tableDataForDeckPriceResp = ref<DeckPriceResp>(NewDeckPriceResp())
 
+let deckJSON = ref<string>("")
 // 示例JSON：
 // ["Exported from http://digimon.card.moe","ST1-01","ST1-03","ST1-03","ST1-03","ST1-06","ST1-06","ST1-07","ST1-07","ST1-07","ST1-07","ST1-16","ST1-16","BT1-010","BT1-010","BT1-020","BT1-025","BT1-025","BT1-084","BT1-085","BT1-114","BT1-114","P-009","P-009","P-009","P-009","BT4-017","BT4-019","BT4-019","BT4-092","BT4-099","BT5-001","BT5-001","BT5-001","BT5-001","BT5-007","BT5-007","BT5-007","BT5-010","BT5-010","BT5-010","BT5-010","BT5-015","BT5-015","BT5-015","BT5-015","BT5-016","BT5-016","BT5-086","BT5-086","BT5-092","BT5-092","ST7-12","P-035","P-035","P-035"]
-function commitWithOfficialJSON(deck: string) {
-  let req = { deck: deck, envir: "chs" }
+function commitWithDeckJSON(deckJSON: string) {
+  let req = { deck: deckJSON, envir: "chs" }
   postDeckPrice(req).then((resp) => {
     tableDataForDeckPriceResp.value.data = resp.data
     tableDataForDeckPriceResp.value.min_price = resp.min_price
@@ -19,9 +19,10 @@ function commitWithOfficialJSON(deck: string) {
   })
 }
 
+let deckHID = ref<string>("")
 // 示例HID: f078e7f43203337c507850c58ccd2d2312f135a3
-function commitWithHID(hid: string) {
-  let req = hid
+function commitWithDeckHID(deckHID: string) {
+  let req = deckHID
   getDeckPriceWithHID(req).then((resp) => {
     tableDataForDeckPriceResp.value.data = resp.data
     tableDataForDeckPriceResp.value.min_price = resp.min_price
@@ -40,13 +41,13 @@ const sortMinPrice = (a: Data, b: Data) => {
 
 <template>
   <div>
-    卡组HID：<input v-model="hid" type="text" />
-    <button @click="commitWithHID(hid)">提交</button>
+    卡组HID：<input v-model="deckHID" type="text" />
+    <button @click="commitWithDeckHID(deckHID)">提交</button>
   </div>
 
   <div>
-    卡组：<textarea v-model="deck" cols="45" rows="5"></textarea>
-    <button @click="commitWithOfficialJSON(deck)">提交</button>
+    卡组：<textarea v-model="deckJSON" cols="45" rows="5"></textarea>
+    <button @click="commitWithDeckJSON(deckJSON)">提交</button>
   </div>
 
   <div>
