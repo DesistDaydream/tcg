0c6a248a160fd82ee43b00fdef3a6809113653f7
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-11-13 11:40:11 +0800

    Update core.ts

--- src/api/v1/core/core.ts
+++ src/api/v1/core/core.ts
@@ -1,8 +1,8 @@
 import axios, { type AxiosRequestConfig, type AxiosResponse } from "axios"
 
 const instance = axios.create({
-  // baseURL: "https://tcg.102205.xyz:8443/api/v1",
-  baseURL: "http://localhost:52205/api/v1",
+  baseURL: "https://tcg.102205.xyz:8443/api/v1",
+  // baseURL: "http://localhost:52205/api/v1",
   timeout: 15000,
 })
 
